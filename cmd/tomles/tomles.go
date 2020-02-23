dec51c4abba1be4aac0a5cecca26fe173ae7580c
Scott Nichols <snichols@vmware.com>
2020-02-23 08:29:21 -0800

    move new command out of main

--- cmd/tomles/tomles.go
+++ cmd/tomles/tomles.go
@@ -3,21 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/spf13/cobra"
-
 	"github.com/n3wscott/tomles/pkg/commands"
 )
 
 func main() {
-	cmds := &cobra.Command{
-		Use:   "tomles",
-		Short: "A tool to aid in yaml editing.",
-		Run: func(cmd *cobra.Command, args []string) {
-			_ = cmd.Help()
-		},
-	}
-	commands.AddTomlesCommands(cmds)
-
+	cmds := commands.New()
 	if err := cmds.Execute(); err != nil {
 		log.Fatalf("error during command execution: %v", err)
 	}
--- pkg/commands/commands.go
+++ pkg/commands/commands.go
@@ -4,6 +4,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func AddTomlesCommands(topLevel *cobra.Command) {
-	addUpdate(topLevel)
+func New() *cobra.Command {
+	cmds := &cobra.Command{
+		Use:   "tomles",
+		Short: "A tool to aid in yaml editing.",
+		Run: func(cmd *cobra.Command, args []string) {
+			_ = cmd.Help()
+		},
+	}
+	addUpdate(cmds)
+
+	return cmds
 }
